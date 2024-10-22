2bd6b52150fa2535435bbf3bb63449d9e1b609b0
Godswill Nasara <godswillnlm@gmail.com>
2024-10-22 16:09:07 +0100

    feature: add ALE analysis plugin

---
+++ nvim/.config/nvim/lua/plugins/ale-analysis.lua
@@ -0,0 +1,26 @@
+return {
+  'dense-analysis/ale',
+  config = function()
+    -- Configuration goes here.
+    local g = vim.g
+
+    g.ale_ruby_rubocop_auto_correct_all = 1
+
+    g.ale_linters = {
+      ruby = { 'rubocop', 'ruby' },
+      lua = { 'lua_language_server' },
+      python = { 'flake8', 'pyright' },
+      javascript = { 'eslint', 'tsserver' },
+      typescript = { 'eslint', 'tsserver' },
+      typescriptreact = { 'eslint', 'tsserver' },
+      json = { 'jsonls' },
+      markdown = { 'alex' },
+      html = { 'htmlhint' },
+      gleam = { 'gleam' },
+      elixir = { 'elixir-ls' },
+      erlang = { 'erlangls' },
+      go = { 'gopls' },
+      dockerfile = { 'hadolint' }
+    }
+  end
+}
