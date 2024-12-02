724b21cae2f99c3b91a4c6b17dad3628648ddf33
Godswill Nasara <godswillnlm@gmail.com>
2024-12-02 02:37:53 +0100

    update(config): add config to bufferline layout

--- nvim/.config/nvim/lua/others/options.lua
+++ nvim/.config/nvim/lua/others/options.lua
@@ -1,2 +1,40 @@
-vim.opt.termguicolors = true -- bufferline
-require("bufferline").setup{} -- bufferline
+vim.opt.termguicolors = true  -- bufferline
+require("bufferline").setup { -- bufferline
+  options = {
+    mode = "buffers",
+    buffer_close_icon = '', -- '󰅖'
+    close_icon = '',
+    diagnostics = "nvim_lsp",
+    diagnostics_indicator = function(count, level, diagnostics_dict, context)
+      local s = " "
+      for e, n in pairs(diagnostics_dict) do
+        local sym = e == "error" and " "
+            or (e == "warning" and " " or " ")
+        s = s .. n .. sym
+      end
+      return s
+    end,
+    separator_style = "slant",    -- | "slope" | "thick" | "thin" | { 'any', 'any' },
+    enforce_regular_tabs = false, -- | true,
+    offsets = {
+      {
+        filetype = "NvimTree",
+        text = "File Explorer",
+        text_align = "center", -- | "left" | "right"
+        highlight = "Directory",
+        separator = true
+      }
+    },
+    hover = {
+      enabled = true,
+      delay = 200,
+      reveal = { 'close' }
+    },
+    sort_by = 'insert_at_end', -- | 'insert_after_current' | 'id' | 'extension' | 'relative_directory' | 'directory' | 'tabs'
+  },
+}
+
+-- diagnostics_indicator = function(count, level)
+--   local icon = level:match("error") and " " or " "
+--   return " " .. icon .. count
+-- end,
