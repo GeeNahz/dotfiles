8b1a51d232885396596a713d849c19b1e40e3e3d
Godswill Nasara <godswillnlm@gmail.com>
2025-12-22 10:55:13 +0100

    feat: modify lua script to install elixir LSP

--- nvim/.config/nvim/lua/plugins/lsp-zero.lua
+++ nvim/.config/nvim/lua/plugins/lsp-zero.lua
@@ -97,7 +97,7 @@ return {
       -- local lspconfig = require('lspconfig')
 
       require('mason-lspconfig').setup({
-        ensure_installed = { "lua_ls", "cssls", "html", "ts_ls", "tailwindcss", "vuels", "pyright", "pylsp", "ast_grep", "docker_compose_language_service", "dockerls", },
+        ensure_installed = { "lua_ls", "cssls", "html", "ts_ls", "tailwindcss", "vuels", "pyright", "pylsp", "ast_grep", "docker_compose_language_service", "dockerls", "elixirls", },
         handlers = {
           -- this first function is the "default handler"
           -- it applies to every language server without a "custom handler"
--- nvim/.config/nvim/lua/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -9,7 +9,23 @@ return {
     require("nvim-tree").setup {
       view = {
         relativenumber = true,
-      }
+      },
+      diagnostics = {
+        enable = true,
+        show_on_dirs = true,
+        show_on_open_dirs = true,
+        debounce_delay = 500,
+        severity = {
+          min = vim.diagnostic.severity.HINT,
+          max = vim.diagnostic.severity.ERROR,
+        },
+        icons = {
+          hint = "",
+          info = "",
+          warning = "",
+          error = "",
+        },
+      },
     }
   end,
 }
